3636db7926967e452fb3fb315dbe4575371d8d6a
TBM-MAHI <tasnimul.bin.moshiur@g.bracu.ac.bd>
2023-03-03 10:39:07 +0600

    added search functionality

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
 
 class App extends React.Component {
   constructor() {
+    console.log("1.constructor");
     super();
     this.state = {
-      name: {
-        firstName:'MAHI',
-        lastName:'ooooo',
-      },
-      company:'ZTM'
-    }
+      monsters: [],
+      searchbox: "",
+    };
   }
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p> Hi I'm {this.state.name.firstName} {this.state.name.lastName} i WORK AT {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(
-              () => {
-                return {
-                  name: { firstName: "becky", lastName: "lynch" },
-                  company: "wwe",
-                };
-              }
-            )
+
+  componentDidMount() {
+    console.log("3.componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) =>
+        this.setState(
+          () => {
+            return { monsters: user };
+          },
+          () => {
+            //return console.log(this.state);
           }
-        }>
-            Change name
-          </button>
-        </header>
+        )
+      );
+  }
+
+  render() {
+    console.log("2.render");
+    const filteredMonsters = this.state.monsters.filter((m) =>
+      m.name.toLowerCase().includes(this.state.searchbox)
+    );
+    return (
+
+      <div className="App">
+        <input
+          className="seearch-box"
+          type="search"
+          placeholder="search..."
+          onChange={(event) => {
+            let searchString = event.target.value.toLowerCase();
+            console.log(searchString);
+           this.setState(() => {
+              return { searchbox: searchString };
+            })
+          }}
+        />
+        { 
+          filteredMonsters.map((m) => <h1 key={m.id}>{m.name}</h1>)
+          
+        }
       </div>
     );
   }
